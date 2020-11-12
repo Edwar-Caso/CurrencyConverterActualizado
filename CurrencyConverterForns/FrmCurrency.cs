50f33a840f88308b87c51e01a14e7a7da9b3865a
Edwar <Edwar@DESKTOP-64VTE1L>
2020-11-12 09:15:27 -0500

    Agregar archivos de proyecto.

---
+++ CurrencyConverterForns/FrmCurrency.cs
@@ -0,0 +1,59 @@
+﻿using CurrencyConverterLibreria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverterForms
+{
+    public partial class FrmCurrency : Form
+    {
+        public FrmCurrency()
+        {
+            InitializeComponent();
+        }
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            decimal converted = 0.0M;
+            decimal initial = 0.0M;
+
+            CurrencyType fromCur = CurrencyType.US;
+            CurrencyType toCur = CurrencyType.US;
+
+            initial = Convert.ToDecimal(Amount.Text);
+
+            if (FromUK.Checked)
+            {
+                fromCur = CurrencyType.UK;
+            }
+            else if (FromAUS.Checked)
+            {
+                fromCur = CurrencyType.AUS;
+            }
+
+            if (ToUK.Checked)
+            {
+                toCur = CurrencyType.UK;
+            }
+            else if (ToAus.Checked)
+            {
+                toCur = CurrencyType.AUS;
+            }
+            converted = CurrencyConvert(initial, fromCur, toCur);
+            Result.Text = converted.ToString();
+        }
+        public static decimal CurrencyConvert(decimal amount, CurrencyType fromCur, CurrencyType toCur)
+        {
+            decimal converted = 0.0M;
+            ConvertibleCurrency currency = new ConvertibleCurrency(fromCur, amount);
+            converted = currency.ConvertTo(toCur);
+            return converted;
+        }
+
+    }
+}
---
+++ CurrencyConverterTests/ConvertTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using CurrencyConverterForms;
+using CurrencyConverterLibreria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyConverterTests
+{
+
+    [TestClass]
+    public class ConvertTest
+    {
+        [TestMethod]
+        public void ConversionBreadth()
+        {
+            decimal result;
+            decimal amount;
+            CurrencyType fromCur;
+            CurrencyType toCur;
+
+            amount = 100.0M;
+            fromCur = CurrencyType.US;
+            toCur = CurrencyType.US;
+            result = FrmCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "US to US should be no change");
+
+            fromCur = CurrencyType.UK;
+            toCur = CurrencyType.UK;
+            result = FrmCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "UK to UK should be no change");
+
+            fromCur = CurrencyType.AUS;
+            toCur = CurrencyType.AUS;
+            result = FrmCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "AUS to AUS should be no change");
+
+            decimal expected;
+            fromCur = CurrencyType.US;
+            toCur = CurrencyType.AUS;
+            result = FrmCurrency.CurrencyConvert(amount, fromCur, toCur);
+            expected = amount * 2;
+            Assert.AreEqual(expected, result, "US to AUS is incorrect");
+
+            fromCur = CurrencyType.UK;
+            toCur = CurrencyType.AUS;
+            result = FrmCurrency.CurrencyConvert(amount, fromCur, toCur);
+            expected = amount / 0.5M * 2;
+            Assert.AreEqual(expected, result, "UK to AUS is incorrect");
+        }
+
+        [TestMethod]
+        public void ConvertTo()
+        {
+            ConvertibleCurrency currency;
+            decimal result;
+            decimal expected;
+
+            currency = new ConvertibleCurrency(CurrencyType.US, 100.0M);
+            result = currency.ConvertTo(CurrencyType.US);
+            Assert.AreEqual(100.0M, result, "US to US should be no change");
+
+            currency = new ConvertibleCurrency(CurrencyType.AUS, 100.0M);
+            result = currency.ConvertTo(CurrencyType.UK);
+            expected = 100.0M / 2 * 0.5M;
+            Assert.AreEqual(expected, result, "AUS to UK incorrect result");
+        }
+
+    }
+}
